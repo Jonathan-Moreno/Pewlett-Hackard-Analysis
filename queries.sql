f5034b37333d5b7435aae081514cc251c2a455e8
Jonathan-Moreno <jonathanpmoreno2015@gmail.com>
2022-02-25 11:49:03 -0800

    created tables for  'current_emp' and 'retiring_emp'

--- queries.sql
+++ queries.sql
@@ -16,3 +16,25 @@ INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Create new table from joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_employees as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+SELECT * FROM current_emp
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO retiring_emp
+FROM current_emp as ce
+LEFT JOIN dept_employees as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
